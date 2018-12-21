c3d3a5ead1521f2aaef95f4c77fc2badf04baf81
Stephanie <stephanie@wabow.com>
2018-12-21 11:31:46 +0800

    update gulp.task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
 var scss = require('gulp-sass');
 
-gulp.task('sass', function() {
+// 單一寫法
+// gulp.task('sass', function() {
 
-  return gulp.src('resourse/sass/*.scss')
-          .pipe(scss())
-          .pipe(gulp.dest('assets/css'))
+//   return gulp.src('resourse/sass/*.scss')
+//           .pipe(scss())
+//           .pipe(gulp.dest('assets/css'))
 
-});
+// });
+
+// 黑名單
+// gulp.task('sass', function() {
+//     return gulp
+//         .src([
+//                 'resourse/sass/**/*.scss',
+//                 '!resourse/sass/editor/**/*.scss'  // 多一組/**/意即連子資料夾也排外
+//             ], { nodir: true })  // If true, only matches files, not directories.
+//         .pipe(scss())
+//         .pipe(gulp.dest('public/css'))
+// });
 
 gulp.task('default', ['sass'], function() {
     gulp.watch('resourse/sass/**/*.scss', ['sass']);
